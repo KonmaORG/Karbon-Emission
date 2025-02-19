204be7fc0ba522ad377bb25aa4b03558d7364630
Cryptonean <faizanvit8@gmail.com>
2025-02-19 10:41:11 +0530

    datum type

--- client/types/cardano.ts
+++ client/types/cardano.ts
@@ -8,3 +8,19 @@ export type Wallet = {
   icon: string;
   enable(): Promise<WalletApi>;
 };
+
+//#region Alias
+export const PaymentKeyHashSchema = Data.Bytes();
+export const StakeKeyHashSchema = Data.Bytes();
+
+export const AddressSchema = Data.Tuple([
+  PaymentKeyHashSchema,
+  StakeKeyHashSchema,
+]);
+//#endregion
+
+export const CETDatum = Data.Object({
+  location: Data.Bytes(),
+  cet_qty: Data.Integer(),
+  time: Data.Integer(),
+});
